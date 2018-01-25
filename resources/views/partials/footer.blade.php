439f29ed5f5081017ad3b18f40fa21f92f0a536c
Yaroslav Kabai <yakabay86@gmail.com>
2018-01-25 17:54:49 +0200

    add blog.scc

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="card-footer text-muted text-center mt-4">
+<footer class="blog-footer text-muted">
     <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
     <p>
         <a href="#">Back to top</a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,5 @@
 @extends('layouts.master')
 
-@section('styles')
-    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
-    {{--<link href="blog.css" rel="stylesheet">--}}
-@endsection
-
 @section('scripts')
     <script>
         Holder.addTheme('thumb', {
